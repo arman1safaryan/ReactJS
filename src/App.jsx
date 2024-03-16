3cb6321ec4f1baddd04ada810538030373aaf97f
arman1safaryan <arman1safaryan@gmail.com>
2024-03-16 15:31:43 +0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import {App2} from './App2';
-import './App.css';
+import { App2 } from "./App2";
 
-
-function App() {
-
+const App = () => {
+  
   return (
-    
-    <div className='App'>
+    <div className="App">
       <App2 />
     </div>
-    
-
   );
-}
+};
 
 export default App;
--- src/App2.jsx
+++ src/App2.jsx
@@ -1,78 +1,35 @@
 import './App.css';
-import  {useState} from 'react';
+import  {useState, useEffect} from 'react';
 
 
 export function App2() {
+    let [mode, setMode] = useState(
+        JSON.parse(localStorage.getItem("mode"))? JSON.parse(localStorage.getItem("mode")) : "white")
 
-    let [mode, setMode] = useState('showStart')
-    let [timer, setTimer] = useState('false')
-    let [second, setSecond] = useState(0)
-    let [minute, setMinute] = useState(0)
-    let [hour, setHour] = useState(0)
-
-
-    function startFunc(){
-        setTimer(true); 
-        setMode('showStop');
-        stopWatchProgress()
+    const click = color => {
+        setMode(color)
     }
 
-    function stopFunc(){
-        setTimer(false);
-        setMode('showReset')
-    }
+    useEffect(() => {
+        document.body.style.backgroundColor = mode
+        localStorage.setItem("mode", JSON.stringify(mode))
+    },[mode])
 
-    function resetFunc(){
-        setMode('showStart')
-        setHour(0); 
-        setMinute(0); 
-        setSecond(0); 
-    }
-
-    function resumeFunc(){
-        setTimer(true);
-        setMode('showStop')
-    }
-
-    
-    function stopWatchProgress() { 
-        if (timer) { 
-            setSecond(second++); 
-     
-            if (second === 60) { 
-                setMinute(minute++); 
-                setSecond(0);
-            } 
-      
-            if (minute === 60) { 
-                setHour(hour++);
-                setMinute(0); 
-                setSecond(0); 
-            } 
-            setTimeout(stopWatchProgress, 1000); 
-        } 
-    }
-  
-    const start = <button class="btn" id="start" onClick={startFunc}> Start</button>
-    const stop = <button class="btn" id="stop" onClick={stopFunc}> Stop</button>
-    const reset = (
-            <>
-            <button class="btn" id="reset" onClick={resetFunc}> Reset </button>
-            <button class="btn" id="resume" onClick={resumeFunc}> Resume </button>
-            </>
-        );
 
     return (
         <>
-            <h1>Stopwatch</h1>
-            <div id="time"> 
-                <span class="digit" id="hr">{hour}:</span> 
-                <span class="digit" id="min">{minute}:</span> 
-                <span class="digit" id="sec">{second}</span> 
-            </div> 
-            <div className='App3'>
-            {mode === 'showStart'? start: mode === 'showStop'? stop: reset}
-            </div>
+        {mode === "white" && (
+            <div className='App'>
+            <h1>React Light Mode</h1>
+            <div><button onClick={() => click("black")}>Dark Mode</button></div>
+        </div>
+        )}
+        {mode === "black" && (
+            <div className=' App DarkMode'>
+            <h1>React Dark Mode</h1>
+            <div><button onClick={() => click("white")}>Dark Mode</button></div>
+        </div>
+        )}
         </>
     );
 }
